d8fa4e387bcdf20870df0ccb2dec88e1e659deea
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-06-23 13:43:16 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	long int A = 0;
+	long int B = 0;
+	long int C = 0;
+	
+	cin >> A >> B >> C;
+	cout << A + B + C << endl;
+
+	return 0;
+
+}
