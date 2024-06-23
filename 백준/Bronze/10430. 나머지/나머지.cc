5d9c7a2c9ad7f3e96ebb77af9aa9bae5dfdf793d
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-06-23 12:52:07 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int A = 0;
+	int B = 0;
+	int C = 0;
+	
+	cin >> A >> B >> C;
+
+	cout << (A + B) % C << endl;
+	cout << ((A % C) + (B % C)) % C << endl;
+	cout << (A * B) % C << endl;
+	cout << ((A % C) * (B % C)) % C << endl;
+
+	return 0;
+}
