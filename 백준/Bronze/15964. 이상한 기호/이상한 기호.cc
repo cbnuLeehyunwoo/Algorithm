c4c2e62afbefc052532ddba8672595b74f19a195
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-08-08 23:02:49 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() {
+    long int A = 0;
+    long int B = 0;
+    cin >> A >> B;
+
+    cout << (A + B) * (A - B);
+
+ 	return 0;
+}
