1d8baadde6c351748b1b6b3c043bf6bd52925113
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-08-24 22:13:03 +0900

    [Unrated] Title: No Description 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/21293. No Description 2/No Description 2.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, r, s, t, u, v, w, x, y, z;
+	cin >> z >> y >> x >> w >> v >> u >> t >> s >> r >> p >> o >> n >> m >> l >> k >> j >> i >> h >> g >> f >> e >> d >> c >> b >> a;
+	cout << a - b << "\n";
+	cout << a * b << "\n";
+	cout << a + b + 1 << "\n";
+	return 0;
+}
