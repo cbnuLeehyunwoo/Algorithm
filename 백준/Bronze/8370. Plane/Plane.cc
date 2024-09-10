a443036430b1c1e25d149e19b1c753816cad5f9b
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-09-11 01:08:27 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,15 @@
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+	int n1 = 0;
+	int n2 = 0;
+	int k1 = 0;
+	int k2 = 0;
+
+	cin >> n1 >> k1 >> n2 >> k2;
+	cout << n1 * k1 + n2 * k2;
+
+	return 0;
+}
