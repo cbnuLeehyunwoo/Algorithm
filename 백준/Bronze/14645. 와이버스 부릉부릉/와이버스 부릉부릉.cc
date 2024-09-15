850d2ef17241121fa16fa20d34d8c005ffb8b834
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-09-15 21:30:08 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N = 0; // 정거장의 수
+	int M = 0; // 거쳐가는 정거장
+	int K = 0; // 탑승자의 수
+	int psngr = 0; // 승객의 총 합
+	
+	cin >> N >> K;
+	for (int i = 0; i < N; i++) {
+		cin >> M >> K;
+	}
+	cout << "비와이";
+}
