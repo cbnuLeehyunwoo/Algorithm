603c6a4776d8c8789a145bc671911cd641e26625
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-10-04 23:50:22 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N = 0;
+	cin >> N;
+	if (N == 0) {
+		cout << "YONSEI";
+	}
+	else {
+		cout << "Leading the Way to the Future";
+	}
+}
