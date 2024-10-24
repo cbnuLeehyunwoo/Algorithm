6f4024936252f63dee1544ce546fcc74fca8b00f
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-10-24 23:32:15 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int N = 0;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cout << "LoveisKoreaUniversity";
+		cout << " ";
+	}
+}
