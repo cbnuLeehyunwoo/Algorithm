f4301e47e2cd85d9d391d9df02868aaee6cd9511
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-10-25 21:52:56 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	 float a = 0;
+	 float b = 0;
+	 cin >> a >> b;
+	 cout << fixed << setprecision(1) << a * b * 1 / 2;
+}
