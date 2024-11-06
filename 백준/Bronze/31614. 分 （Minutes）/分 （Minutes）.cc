24064d1ac51e979ee552fd1b3def613ac5d51a1d
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-11-06 23:58:19 +0900

    [Bronze V] Title: 分 (Minutes), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31614. 分 （Minutes）/分 （Minutes）.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int H, M;
+	cin >> H >> M;
+	cout << 60 * H + M;
+
+}
