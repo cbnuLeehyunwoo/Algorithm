eef8ec3c65ee7e03252fccaefcad063e14dac841
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-11-12 14:49:48 +0900

    [Bronze I] Title: 유진수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1356. 유진수/유진수.cc
@@ -0,0 +1,28 @@
+// 브루트 포스 알고리즘으로 해결
+#include <iostream>
+#include <string>
+
+using namespace std;
+				 
+int main() {
+	unsigned long int N;
+	cin >> N;
+	string str = to_string(N);
+
+	for (int i = 1; i < str.size(); i++) {
+		int res1 = 1;
+		int res2 = 1;
+		for (int j = 0; j < i; j++) {
+			res1 = res1 * (str[j] - '0');
+		}
+		for (int k = i; k < str.size(); k++) {
+			res2 = res2 * (str[k] - '0');
+		}
+		if (res1 == res2) {
+			cout << "YES";
+			return 0;
+		}
+	}
+	cout << "NO";
+	return 0;
+}
