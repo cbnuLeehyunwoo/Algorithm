5dcae21f46d17bda61e71f0495ba10c7c0bd4947
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-11-29 23:11:57 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int month, day;
+	cin >> month >> day;
+	if (month == 2 && day == 18) {
+		cout << "Special";
+	}
+	else if (month < 2) {
+		cout << "Before";
+	}
+	else if (month <= 2 && day < 18) {
+		cout << "Before";
+	}
+	else if (month > 2) {
+		cout << "After";
+	}
+	else if (month >= 2 && day > 18) {
+		cout << "After";
+	}
+}
