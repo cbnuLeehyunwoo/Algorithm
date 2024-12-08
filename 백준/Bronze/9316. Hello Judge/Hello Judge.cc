bc8e44d99590d171bf6a246c8bb4a13a457d9b68
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-12-08 17:51:22 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+int main() {
+	int N = 0;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		cout << "Hello " << "World, " << "Judge " << i << "!\n";
+	}
+}
