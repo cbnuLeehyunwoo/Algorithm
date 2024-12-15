55d11f7a10e71aab5262c7d1f60ea660e4a7a4a0
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-12-15 15:52:24 +0900

    [Bronze IV] Title: 달달함이 넘쳐흘러, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+int main() {
+	int ax, ay, az;
+	int bx, by, bz;
+	int cx, cy, cz;
+
+	cin >> ax >> ay >> az;
+	cin >> cx >> cy >> cz;
+
+	bx = cx - az;
+	by = cy / ay;
+	bz = cz - ax;
+
+	cout << bx  << " " <<  by << " " << bz;
+}
