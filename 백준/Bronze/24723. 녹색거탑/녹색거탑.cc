ed5a51950c07684f3dceac7c2aa4beb9d1e60cd5
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2024-12-21 12:21:45 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	int N = 0;
+	cin >> N;
+	//int count = 0;
+	//for (int i = 1; i <= N; i++) {
+	//	count = (i * 2);
+	//}
+	cout << pow(2, N);
+}
