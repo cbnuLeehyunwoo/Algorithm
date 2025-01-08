11797f01e71e20bebc0c417e1aafa6abbc432277
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-01-08 14:13:38 +0900

    [Bronze IV] Title: 이 대회는 이제 제 겁니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31922. 이 대회는 이제 제 겁니다/이 대회는 이제 제 겁니다.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a, p , c;
+    cin >> a >> p >> c;
+    if(a + c > p) {
+        cout << a + c;
+    }
+    else 
+        cout << p;
+
+}
