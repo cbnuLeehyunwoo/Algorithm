f18c367be5ebbe84f45d9e8a2567cd6be140ce25
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-01-13 15:26:30 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int n = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		long int a, b;
+		cin >> a >> b;
+		cout << a + b << "\n";
+	}
+
+}
+
+  
