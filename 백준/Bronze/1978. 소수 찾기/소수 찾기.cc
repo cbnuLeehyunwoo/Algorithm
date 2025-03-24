02f3ac69c6bfb4fa04d6b9f7b5f080f5fe4b6abe
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-03-24 17:44:53 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
@@ -1,35 +1,24 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-int main()
-{
-	int N = 0;
-	int prime_N = 0;
-	cin >> N;
-	int N_arr[100] = { 0, };
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	for (int i = 0; i < N; i++) {
-		cin >> N_arr[i];
-		bool isPrime = true;
+    int N, num, prime_N = 0;
+    cin >> N;
 
-		if (N_arr[i] <= 1) {
-			isPrime = false;
-		}
-		else {
-			for (int j = 2; j * j <= N_arr[i]; j++) {    // 각각의 수의 제곱근 까지만 나누어 떨어지는지 확인해도 문제없다. 
-				if (N_arr[i] % j == 0) {
-					isPrime = false;
-					break;
-				}
-			}
-		}
+    while (N--) {
+        cin >> num;
+        bool isPrime = (num > 1);
 
-		if (isPrime) {
-			prime_N++;
-		}
-	}
+        for (int j = 2; isPrime && j * j <= num; j++) {
+            if (num % j == 0) isPrime = false;
+        }
 
-	cout << prime_N;
-	return 0;
+        if (isPrime) prime_N++;
+    }
+
+    cout << prime_N;
+    return 0;
 }
