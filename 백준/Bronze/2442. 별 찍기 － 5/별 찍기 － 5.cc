49728b5f272608bf91bb5cb83749c2575aa61b8f
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-03-25 21:02:32 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <cmath>
+
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     int N = 0;
     int index = 0;
     cin >> N;
