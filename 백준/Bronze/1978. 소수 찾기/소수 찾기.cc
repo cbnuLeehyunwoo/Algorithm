eb49b759e06350435d40d23d7792bfd6c083a9a5
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-03-30 14:11:16 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
@@ -1,24 +1,26 @@
 #include <iostream>
 using namespace std;
 
+bool is_prime(int n) {
+	if (n < 2) return false;
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0) return false;
+	}
+	return true;
+}
+
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    int N, num, prime_N = 0;
-    cin >> N;
-
-    while (N--) {
-        cin >> num;
-        bool isPrime = (num > 1);
-
-        for (int j = 2; isPrime && j * j <= num; j++) {
-            if (num % j == 0) isPrime = false;
-        }
-
-        if (isPrime) prime_N++;
-    }
-
-    cout << prime_N;
-    return 0;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	int cnt = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int temp;
+		cin >> temp;
+		if (is_prime(temp)) {
+			cnt++;
+		}
+	}
+	cout << cnt;
 }
