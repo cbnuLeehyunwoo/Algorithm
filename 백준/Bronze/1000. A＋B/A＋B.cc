4d3e2eaa8208c87c93ced1c3f1aa82ef919a98ea
cbnuLeehyunwoo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-04-04 19:42:28 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,13 +1,9 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
-    int a = 0;
-    int b = 0;
-    
-    
-    cin >> a >> b;
-    cout << a + b << endl;
-    return 0;
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a + b;
 }
