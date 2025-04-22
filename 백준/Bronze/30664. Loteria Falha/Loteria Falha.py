9241319ba31901ba8498b3847ef78bbb0e014fc1
Lee Hyun Woo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-04-22 22:45:34 +0900

    [Bronze V] Title: Loteria Falha, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30664. Loteria Falha/Loteria Falha.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+while(True):
+    temp = int(input())
+    if(temp == 0):
+        break
+    if(temp % 42 == 0):
+        print("PREMIADO")
+    else:
+        print("TENTE NOVAMENTE")
