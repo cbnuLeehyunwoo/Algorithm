aaafecfd606b4c8ee8e1aa7f24923799888ebf82
Lee Hyun Woo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-05-05 20:57:00 +0900

    [Silver IV] Title: 괄호, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -4,35 +4,31 @@
 using namespace std;
 
 int main() {
-	string VPS[10000];
-	int N = 0; // 입력받을 문자열의 갯수
+	int N;
 	cin >> N;
-	for (int i = 0; i < N; i++) {
-		int x = 0;
-		int y = 0;
-		cin >> VPS[i];
-		for (int j = 0; j < VPS[i].length(); j++) {
-			if (VPS[i][j] == '(') {
-				x++;
+
+	while (N--) {
+		string s;
+		cin >> s;
+		int cnt = 0;
+		bool isValid = true;
+
+		for (char c : s) {
+			if (c == '(') {
+				cnt++;
 			}
-			else if (VPS[i][j] == ')') {
-				y++;
-			}
-			if (y > x) {	// 닫는 괄호의 수가 여는 괄호의 수 보다 커지면 VPS가 될 수 없다.
-				cout << "NO\n";
-				goto next;
+			else if (c == ')') {
+				cnt--;
+				if (cnt < 0) { 
+					isValid = false;
+					break;
+				}
 			}
 		}
-		if (x == y) {		// 둘의 숫자가 같으면 VPS로 판단.
-			cout << "YES\n";
-			continue;
-		}
-		else {
-			cout << "NO\n";
-			continue;
-		}
-next:;
+
+		if (isValid && cnt == 0) cout << "YES\n";
+		else cout << "NO\n";
 	}
-	
+
 	return 0;
 }
