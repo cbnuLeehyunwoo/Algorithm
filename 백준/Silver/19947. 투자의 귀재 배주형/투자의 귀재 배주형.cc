e19e69475df287efeb3dd96876ff1c9c6f9ef2fa
Lee Hyun Woo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-05-08 20:09:21 +0900

    [Silver V] Title: 투자의 귀재 배주형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.cc
+++ 백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.cc
@@ -8,12 +8,12 @@ void build_dp(int x, int y) {
 	dp[0] = x;
 	dp[1] = x * 1.05;
 	dp[2] = dp[1] * 1.05;
-	dp[3] = max(x * 1.20, dp[2] * 1.05);
-	dp[4] = max(dp[3] * 1.05, dp[1] * 1.2);
+	dp[3] = x * 1.20;
+	dp[4] = dp[1] * 1.2;
 	for (int i = 5; i <= y; i++) {
-		unsigned long long temp1 = dp[i - 5] * 1.35;
-		unsigned long long temp2 = dp[i - 3] * 1.2;
-		unsigned long long temp3 = dp[i - 1] * 1.05;
+		int temp1 = dp[i - 5] * 1.35;
+		int temp2 = dp[i - 3] * 1.2;
+		int temp3 = dp[i - 1] * 1.05;
 		dp[i] = max(max(temp1, temp2), temp3);
 	}
 }
