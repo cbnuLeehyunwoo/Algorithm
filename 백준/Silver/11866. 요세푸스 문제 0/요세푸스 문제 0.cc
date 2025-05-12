5069a3af38c052aaf20fad6afc658c87db37f5c8
Lee Hyun Woo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-05-12 22:22:35 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -1,34 +1,28 @@
-#include<iostream>
-#include<algorithm>
-#include<queue>
+#include <iostream>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
+queue<int> q;
 
 int main() {
-	int n = 0;
-	int k = 0;
-
-	queue<int> que;
-	
+	int n, k;
 	cin >> n >> k;
 	for (int i = 1; i <= n; i++) {
-		que.push(i);
+		q.push(i);
 	}
-
 	cout << "<";
-
-	while (que.empty() == 0) {        // 큐 안에 원소가 있을때까지 반복.
+	while (!q.empty()) {
 		for (int i = 1; i < k; i++) {
-			que.push(que.front());   // 1부터 k-1번째 원소까지 큐의 뒤로 보냄
-			que.pop();				 // 보낸 원소를 삭제
+			q.push(q.front());
+			q.pop();
 		}
-		cout << que.front();
-		if (que.size() >= 2) {
+		cout << q.front();
+		q.pop();
+		if (!q.empty()) {
 			cout << ", ";
 		}
-		que.pop();
 	}
-
 	cout << ">";
 	return 0;
 }
