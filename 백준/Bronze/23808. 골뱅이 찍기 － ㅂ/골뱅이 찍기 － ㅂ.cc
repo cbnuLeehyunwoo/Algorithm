b04bfc0e78ad9ad90ee8f1d75f8f7ede00b11ba2
Lee Hyun Woo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-08-08 17:50:20 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅂ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23808. 골뱅이 찍기 － ㅂ/골뱅이 찍기 － ㅂ.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < 2 * n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << "@";
+		}
+		for (int j = 0; j < 3 * n; j++) {
+			cout << " ";
+		}
+		for (int j = 0; j < n; j++) {
+			cout << "@";
+		}
+		cout << "\n";
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << "@";
+		}
+		for (int j = 0; j < 3 * n; j++) {
+			cout << "@";
+		}
+		for (int j = 0; j < n; j++) {
+			cout << "@";
+		}
+		cout << "\n";
+	}
+	for (int i = 0; i <  n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << "@";
+		}
+		for (int j = 0; j < 3 * n; j++) {
+			cout << " ";
+		}
+		for (int j = 0; j < n; j++) {
+			cout << "@";
+		}
+		cout << "\n";
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << "@";
+		}
+		for (int j = 0; j < 3 * n; j++) {
+			cout << "@";
+		}
+		for (int j = 0; j < n; j++) {
+			cout << "@";
+		}
+		cout << "\n";
+	}
+}
