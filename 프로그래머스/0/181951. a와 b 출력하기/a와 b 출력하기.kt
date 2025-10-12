76e0cad551966ac9ae7a2c4deea892893aaf0d79
Lee Hyun Woo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-10-12 23:38:17 +0900

    [level 0] Title: a와 b 출력하기, Time: 181.04 ms, Memory: 49.8 MB -BaekjoonHub

--- 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.kt
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
     val (a, b) = readLine()!!.split(' ').map(String::toInt)
-    println("a = ${a}")
-    println("b = ${b}")
+    println("a = $a")
+    println("b = $b")
 }
