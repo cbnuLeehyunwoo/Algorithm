dacb83f9cfba05ed0ad6e0d1f254191a2d8c984b
Lee Hyun Woo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-10-14 01:32:15 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 11.91 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.kt
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.kt
@@ -1,11 +1,5 @@
 class Solution {
     fun solution(my_string: String, alp: String): String {
-        var answer: String = ""
-        for(c in my_string) {
-            if(c.toString() == alp) answer += c.toUpperCase()
-            else answer += c
-        }
-        
-        return answer
+        return my_string.replace(alp, alp.toUpperCase())
     }
 }
