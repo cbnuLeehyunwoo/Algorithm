46848a71466f92abaa964786858715d0bb56d3ca
Lee Hyun Woo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-10-14 13:58:48 +0900

    [level 0] Title: 배열 뒤집기, Time: 9.71 ms, Memory: 66.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(num_list: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        for(i in num_list.size-1 downTo 0) {
+            answer += num_list[i]
+        }
+        return answer
+    }
+}
