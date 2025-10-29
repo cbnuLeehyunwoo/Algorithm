83dbce41afa6e32de9ff4aa500c2571b9930b483
Lee Hyun Woo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-10-29 17:03:22 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.03 ms, Memory: 63.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(a: Int, d: Int, included: BooleanArray): Int {
+        var answer: Int = 0
+        for((index, element) in included.withIndex()) {
+            if(element == true) answer += index * d + a
+        }
+        return answer
+    }
+}
