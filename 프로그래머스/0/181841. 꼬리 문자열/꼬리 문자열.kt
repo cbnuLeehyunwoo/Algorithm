a6f1bc6139c4e26464fdc091bd5fcd5a51e97adc
Lee Hyun Woo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-12-29 23:03:13 +0900

    [level 0] Title: 꼬리 문자열, Time: 20.08 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.kt
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.kt
@@ -1,11 +1,4 @@
 class Solution {
     fun solution(str_list: Array<String>, ex: String): String {
-        var answer: String = ""
-        str_list.filterNot {
-            it.contains(ex)
-        }.forEach {
-            answer += it
-        }
-        return answer
-    }
+        return str_list.filterNot { it.contains(ex) }.joinToString("") }
 }
