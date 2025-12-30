36d8de4e75c2807d9a16f31da4b987bd59a05b1b
Lee Hyun Woo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-12-30 18:46:38 +0900

    [Silver V] Title: 색종이, Time: 148 ms, Memory: 24400 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.kt
+++ 백준/Silver/2563. 색종이/색종이.kt
@@ -1,16 +1,21 @@
+val wholePaper = Array(100) { IntArray(100) }
+
 fun main() {
-    var paper = Array(100) { IntArray(100) }
-    var total = 0
-    val n = readln().toInt()
-    repeat(n) {
-        val (a, b) = readln().split(" ").map { it.toInt() }
-        for (i in b  until b + 10) {
-            for (j in a  until a + 10) {
-                paper[i][j] = 1
-            }
+    var totalExtent = 0
+    val partPaperNumber= readln().toInt()
+    repeat(partPaperNumber) {
+        val (startX, startY) = readln().split(" ").map { it.toInt() }
+        fillPaper(startX, startY, 10)
+    }
+    val flattenPaper = wholePaper.flatMap { it.toList() }
+    flattenPaper.forEach { if (it == 1) totalExtent++ }
+    print(totalExtent)
+}
+
+fun fillPaper(startX: Int, startY: Int, step: Int): Unit {
+    for(i in startX until startX + step) {
+        for(j in startY until startY + step) {
+            wholePaper[i][j] = 1
         }
     }
-    val flatPaper = paper.flatMap { it.toList() }
-    flatPaper.forEach { if (it == 1) total++ }
-    print(total)
 }
