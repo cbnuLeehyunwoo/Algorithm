c8103fcf624a212ec7fe0f36383dfaa65506cd35
Lee Hyun Woo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2025-12-31 23:00:24 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 116 ms, Memory: 21408 KB -BaekjoonHub

--- 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.kt
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.kt
@@ -1,69 +1,67 @@
 fun main() {
     val seedMoney = readln().toInt()
-    val machineDuckStockPrices = readln().split(" ").map { it.toInt() }
-    val resJunhyun = buyAndPray(seedMoney, machineDuckStockPrices.toMutableList())
-    val resSungmin = threeThree(seedMoney, machineDuckStockPrices.toMutableList())
-    if (resSungmin > resJunhyun) print("TIMING")
-    else if (resSungmin < resJunhyun) print("BNP")
-    else print("SAMESAME")
+    val prices = readln().split(" ").map { it.toInt() }
+
+    val junHyun = Trader(seedMoney)
+    val sugMin = Trader(seedMoney)
+    runBNP(junHyun, prices)
+    runTiming(sugMin, prices)
+
+    if(junHyun.totalAsset(prices.last()) > sugMin.totalAsset(prices.last()))
+        print("BNP")
+    else if(junHyun.totalAsset(prices.last()) < sugMin.totalAsset(prices.last()))
+        print("TIMING")
+    else print ("SAMESAME")
 }
 
-fun buyAndPray(seed: Int, stockPrices: MutableList<Int>): Int {
-    var currentSeed = seed
-    var purchasedCount = 0
+class Trader(
+    seedMoney: Int,
+) {
+    var cash = seedMoney
+    var stockCount = 0
+    fun purchaseAll(stockPrice: Int) {
+        stockCount += (cash / stockPrice)
+        cash %= stockPrice
+    }
 
-    for(price in stockPrices) {
-        if (currentSeed >= price) {
-            val purchaseResult = purchaseAll(currentSeed, price)
-            purchasedCount = purchaseResult.first
-            currentSeed = purchaseResult.second
+    fun divestAll(stockPrice: Int) {
+        cash += stockCount * stockPrice
+        stockCount = 0
+    }
+
+    fun totalAsset(stockPrice: Int): Int {
+        return cash + stockCount * stockPrice
+    }
+}
+
+fun runBNP(trader: Trader, prices: List<Int>) {
+    for(price in prices) {
+        if (trader.cash >= price) {
+            trader.purchaseAll(price)
         }
     }
-    return purchasedCount * stockPrices.last() + currentSeed
 }
 
-fun threeThree(seed: Int, stockPrices: MutableList<Int>): Int {
-    var currentSeed = seed
+fun runTiming(trader: Trader, prices: List<Int>) {
     var upStreak = 0
     var downStreak = 0
-    var beforePrice = stockPrices.first()
-    var purchasedCount = 0
-    for (i in stockPrices.indices) {
-        if (stockPrices[i] > beforePrice) {
-            upStreak++
-            downStreak = 0
-        } else if (stockPrices[i] < beforePrice) {
-            downStreak++
-            upStreak = 0
-        } else {
-            upStreak = 0
-            downStreak = 0
+    var prevPrice = prices.first()
+    for(price in prices) {
+        when {
+            price > prevPrice -> { upStreak++; downStreak = 0 }
+            price < prevPrice -> { downStreak++; upStreak = 0 }
+            else -> { upStreak = 0; downStreak = 0 }
         }
-
-        if (downStreak == 3) {
-            val purchaseResult = purchaseAll(currentSeed, stockPrices[i])
-            purchasedCount += purchaseResult.first
-            currentSeed = purchaseResult.second
+        if(downStreak == 3) {
+            upStreak = 0
             downStreak = 2
-        } else if (upStreak == 3) {
-            val divestResult = divestAll(purchasedCount, stockPrices[i])
-            currentSeed += divestResult
-            purchasedCount = 0
-            upStreak = 2
+            trader.purchaseAll(price)
         }
-        beforePrice = stockPrices[i]
+        else if(upStreak == 3) {
+            downStreak = 0
+            upStreak = 2
+            trader.divestAll(price)
+        }
+        prevPrice = price
     }
-    return currentSeed + (purchasedCount * stockPrices.last())
 }
-
-fun purchaseAll(seed: Int, stockPrice: Int): Pair<Int, Int> {
-    val buyCount = seed / stockPrice
-    val resSeed = seed % stockPrice
-    val purchaseResult = Pair(
-        buyCount,
-        resSeed
-    )
-    return purchaseResult
-}
-
-fun divestAll(purchasedCount: Int, stockPrice: Int) = purchasedCount * stockPrice
