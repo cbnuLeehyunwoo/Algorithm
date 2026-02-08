5e46170deb2bb9f4dfe617bbaf2829d745778dbc
Lee Hyun Woo <85469621+cbnuLeehyunwoo@users.noreply.github.com>
2026-02-09 01:15:29 +0900

    [Silver III] Title: 이친수, Time: 76 ms, Memory: 12332 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val n = readln().toInt()
+
+    val dp = Array(n + 1) { LongArray(2) }
+
+    dp[1][1] = 1  // "1"
+
+    for (i in 2..n) {
+        dp[i][0] = dp[i - 1][0] + dp[i - 1][1]
+        dp[i][1] = dp[i - 1][0]
+    }
+
+    println(dp[n][0] + dp[n][1])
+}
